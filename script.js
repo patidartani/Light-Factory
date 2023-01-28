bf56e0d279e8740be8051db74df22ea087f25fce
patidartani <tanishapatidar905@gmail.com>
2023-01-28 20:17:48 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+//    ------------Loading--start------
+ var preloader = document.getElementById('loading');
+
+ function myFunction(){
+          preloader.style.display = 'none';
+ }
+
+//    ------------Loading--end------
